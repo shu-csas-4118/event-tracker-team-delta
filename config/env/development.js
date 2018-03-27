981dd24a9db4183e9ce14c834148a2ea2c5cb191
Garett Chang <changgar@shu.edu>
2018-03-27 16:57:44 -0400

    First commit of project files

---
+++ config/env/development.js
@@ -0,0 +1,17 @@
+﻿module.exports = {
+    debug: true,
+    cookieSecret: 'f1e55aec-c9c0-4639-bf47-f4a0e3e1618f',
+    server: {
+        host: "127.0.0.1",
+        port: 8081
+    },
+    mongodb: {
+        url: ""
+    },
+    logger: {
+        logLevel:  'debug', // Default to this if the environment variable LOG_LEVEL is not set
+        logPath: '/var/log',
+        errorLog: 'errors.log',
+        debugLog: 'debug.log'
+    }
+};
---
+++ config/index.js
@@ -0,0 +1,19 @@
+﻿module.exports = (function (env) {
+    var config = {};
+
+    switch(env) {
+        case 'test':
+            config = require('./env/test');
+            break;
+        case 'production':
+            config = require('./env/production');
+            break;
+        case 'development':
+        default:
+            console.log('No environment set. Defaulting to development');
+            config = require('./env/development');
+            break;;
+    }
+
+    return config;
+})(process.env.NODE_ENV);
