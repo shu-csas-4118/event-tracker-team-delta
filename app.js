95058aa172b83880b1d6108519ec2c4432a246a5
josdes <35999990+josdes@users.noreply.github.com>
2018-04-04 08:25:48 -0400

    Hi everyone ^_^ This is gonna make it work. I updated my packages and changed app.js

--- app.js
+++ app.js
@@ -41,6 +41,16 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+  });
+
+  var server = app.listen(8081, function() {
+  	console.log('Listening on port 8081');
+  });
+
+  server.setTimeout(0, function(){
+  	console.log('Server timed out');
+  });
+
+
 
 module.exports = app;
