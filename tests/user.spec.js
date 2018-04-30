0685e76f3852aac313394be242e7c8abbc024509
josdes <35999990+josdes@users.noreply.github.com>
2018-04-30 15:27:22 -0400

    help
    whats happening

---
+++ tests/user.spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const expect = require ('chai').expect
+const mongoose = require('mongoose');
+const Account = require("../mondels/account");
+
+describe('Tests for user account', () => {
+    before((done) => {
+        const db = mongoose.connect('mongodb://localhost/eventtrack');
+        done();
+    });
+
+    after((done) =>{
+        mongoose.connection.close();
+        done();
+    });
+
+    beforeEach( (done) => {
+        var account = new Account({
+            username: 'john.doe@shu.edu',
+            password: 'password'
+        });
+
+        account.save((error) => {
+            if(error) console.log('error ' + error.message);
+            done();
+        });
+    });
+
+    it('Find a user by their username', (done) => {
+        Account.findOne({ username: 'john.doe@shu.edu' }, (err, account) => {
+            expect(account.username).to.eql('john.doe@shu.edu');
+            done();
+        });
+    });
+
+    afterEach((done) => {
+        Account.remove({}, () => {
+            done();
+        });
+    });
+});
+
