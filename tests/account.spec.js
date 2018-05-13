1b3d7e3d6dc907db04ad5465ea68d98448090254
Garrett Denton <dentonga@shu.edu>
2018-05-13 13:59:30 -0400

    Possible Tests
    These tests work and pass. Any more I should add/test? Also, left the old test stuff there in case what I did is totally wrong.

--- tests/account.spec.js
+++ tests/account.spec.js
@@ -4,7 +4,7 @@ const expect = require ('chai').expect
 const mongoose = require('mongoose');
 const Account = require("../models/account");
 
-describe('Tests for user account', () => {
+/*describe('Tests for user account', () => {
     before((done) => {
         const db = mongoose.connect('mongodb://localhost/eventtrack');
         done();
@@ -53,5 +53,62 @@ describe('Tests for user account', () => {
             
         })
     }) 
-}); 
+}); */
 
+describe('Account tests', () => {
+    before((done) => {
+        const db = mongoose.connect('mongodb://localhost/eventtrack');
+        done();
+    });
+
+    after((done) =>{
+        mongoose.connection.close();
+        done();
+    });
+
+    beforeEach((done) => {
+        var account = new Account({
+            username: 'john.doe@shu.edu',
+            password: 'password'
+        });
+
+        account.save((error) => {
+            if(error) console.log('error ' + error.message);
+            done();
+        });
+    });
+
+    it('Find a user by their username', (done) => {
+        Account.findOne({ username: 'john.doe@shu.edu' }, (err, account) => {
+            expect(account.username).to.eql('john.doe@shu.edu');
+        });
+        done();
+    });
+
+    it('Verify user password', (done) => {
+        Account.findOne({ username: 'john.doe@shu.edu'},(err, account) => {
+            expect(account.password).to.eql('password');
+        });
+        done();
+    });
+
+    it('Username expects a string', (done) => {
+        Account.findOne({ username: 'john.doe@shu.edu' }, (err, account) => {
+            expect(account.username).to.be.a('string');
+        });
+        done();
+    });
+    
+    it('Password expects a string', (done) => {
+        Account.findOne({ username: 'john.doe@shu.edu'},(err, account) => {
+            expect(account.password).to.be.a('string');
+        });
+        done();
+    });
+
+    afterEach((done) => {
+        Account.remove({}, () => {
+            done();
+        });
+    });
+});
--- tests/event.spec.js
+++ tests/event.spec.js
@@ -4,9 +4,7 @@ const Event = require('../models/event')
 const mongoose = require('mongoose');
 const expect = require('chai').expect
 
-const event = new Event();
-
-describe('Event module', ()  => {
+/*describe('Event module', ()  => {
     describe('"seed"', () => {
         it('should have a getEvents method', () => {
             expect(event.seed).to.be.a('function');
@@ -56,5 +54,51 @@ describe('Event module', ()  => {
             expect(e.attendees).to.eql([]);
         });
     });
-})
+})*/
 
+describe('Event tests', () => {
+    before((done) => {
+        const db = mongoose.connect('mongodb://localhost/eventtrack');
+        done();
+    });
+
+    after((done) =>{
+        mongoose.connection.close();
+        done();
+    });
+
+    beforeEach((done) => {
+        var event = new Event({
+            date: '10-10-10',
+            time: '4:00',
+            owner: 'John',
+            attd: [],
+            price: '7'
+        });
+
+        event.save((error) => {
+            if(error) console.log('error ' + error.message);
+            done();
+        });
+    });
+
+    it('Find an event by owner', (done) => {
+        Event.findOne({ owner: 'John' }, (err, event) => {
+            expect(event.owner).to.eql('John');
+        });
+        done();
+    });
+
+    it('Owner expects a string', (done) => {
+        Event.findOne({ owner: 'John' }, (err, event) => {
+            expect(event.owner).to.be.a('string');
+        });
+        done();
+    });
+
+    afterEach((done) => {
+        Event.remove({}, () => {
+            done();
+        });
+    });
+});
